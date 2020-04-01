df83e5714c5f121a914a5a212698031aec8537a1
Davi Rezende <daviresendes12@gmail.com>
2020-04-01 18:20:33 -0300

    Banco de dados

--- TesteEnum/src/main/Teste.java
+++ TesteEnum/src/main/Teste.java
@@ -1,10 +1,17 @@
 package main;
 
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 import Corpo.Cargo;
 import Corpo.Funcionario;
 
 public class Teste {
 	public static void main(String[] args ) {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+		Date hora = Calendar.getInstance().getTime();
 		
 		Funcionario fun = new Funcionario(Cargo.DIRETOR, 20.000);
 		
@@ -13,8 +20,7 @@ public class Teste {
 		
 		System.out.println(fun.toString());
 		
-		System.out.println(cDire.ordinal());
-		System.out.println(cCai.ordinal());
+		System.out.println(sdf.format(hora));
 		
 	}
 }
